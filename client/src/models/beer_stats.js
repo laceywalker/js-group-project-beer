f8fcddf4f279bd8ce05656e44e163bde2f8b2063
Thomas Kendrick <tommkendrick@gmail.com>
2019-01-25 15:05:38 +0000

    added logic to stats model

--- client/src/models/beer_stats.js
+++ client/src/models/beer_stats.js
@@ -9,27 +9,32 @@ BeerStats.prototype.bindEvents = function(){
   PubSub.subscribe('Beers:data-loaded', event => {
     this.beerArray = event.detail;
     this.calculateStats();
+    console.table(this.beerStats);
   })
 }
 
 BeerStats.prototype.calculateStats = function(){
 
+  // Various function calls to calculate the individual stats go here
 
-// Various function calls to calculate the individual stats go here
-const numBeersDrank = this.beerArray.length;
+  const numBeersDrank = this.beerArray.length;
 
-this.favouriteBeer();
-this.worstBeer();
-this.favouriteCountry();
-this.averageRating();
-this.averageABV();
+  const favouriteBeer = this.favouriteBeer();
+  const worstBeer = this.worstBeer();
+  const favouriteCountry = this.favouriteCountry();
+  const averageRating = this.averageRating();
+  const averageABV = this.averageABV();
 
-// Package the various stats into an object for publication
+  // Package the various stats into an object for publication
   
-this.beerStats = {'numBeersDrank': numBeersDrank}; 
-
-console.table(this.beerArray);
-console.table(this.beerStats);
+  this.beerStats = 
+    {
+      'numBeersDrank': numBeersDrank,
+      'worstBeer': worstBeer,
+      'favouriteCountry': favouriteCountry,
+      'averageRating': averageRating,
+      'averageABV': averageABV
+    }; 
 }
 
 BeerStats.prototype.favouriteBeer = function(){
@@ -37,7 +42,7 @@ BeerStats.prototype.favouriteBeer = function(){
 }
 
 BeerStats.prototype.worstBeer = function(){
-  return null;
+  return 'Tennents';
 }
 
 BeerStats.prototype.favouriteCountry = function(){
@@ -45,11 +50,24 @@ BeerStats.prototype.favouriteCountry = function(){
 }
 
 BeerStats.prototype.averageRating = function(){
-  return null;
+  // This function takes in a list of beers objects and returns the average rating of each
+  const ratingsArray = this.beerArray.map( beer => beer.rating )
+  const ratingsAvg = (
+    ratingsArray.reduce((acc, rating) => acc + rating) 
+    / ratingsArray.length
+  ).toFixed(2);
+  return ratingsAvg;
+
 }
 
 BeerStats.prototype.averageABV = function(){
-  return null;
+  // This function takes in a list of beer objects and returns the average ABV
+  const abvArray = this.beerArray.map( beer => beer.abv );
+  const abvAvg = (
+    abvArray.reduce((acc, abv) => acc + abv) 
+    / abvArray.length
+  ).toFixed(2);
+  return abvAvg;
 }
 
 module.exports = BeerStats;
