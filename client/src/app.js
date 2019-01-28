340e1c6ba1f9705ea01acdccb6e93806aa4e6755
Thomas Kendrick <tommkendrick@gmail.com>
2019-01-28 21:47:59 +0000

    implemented pubsub

--- client/src/app.js
+++ client/src/app.js
@@ -1,7 +1,7 @@
-import * as am4core from '@amcharts/amcharts4/core';
-import * as am4maps from '@amcharts/amcharts4/maps';
-import am4geodata_worldLow from '@amcharts/amcharts4-geodata/worldLow';
 
+import { bindEventsMap } from './views/mapview.js';
+
+const PubSub = require('./helpers/pub_sub.js');
 
 const Beers = require('./models/beers.js');
 const BeerStats = require('./models/beer_stats.js');
@@ -54,58 +54,8 @@ document.addEventListener('DOMContentLoaded', () => {
   beers.bindEvents();
   beers.getData();
 
-  drawMap();
+  bindEventsMap();
+
 });
-
-const drawMap = function(){
-
-  // Create map instance
-  var chart = am4core.create('chartdiv', am4maps.MapChart);
-
-  // Set map definition
-  chart.geodata = am4geodata_worldLow;
-
-  // Set projection
-  chart.projection = new am4maps.projections.Miller();
-
-  // Create map polygon series
-  var polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
-
-  // Make map load polygon (like country names) data from GeoJSON
-  polygonSeries.useGeodata = true;
-
-  // Configure series
-  var polygonTemplate = polygonSeries.mapPolygons.template;
-  polygonTemplate.tooltipText = '{name}';
-  polygonTemplate.fill = am4core.color('#74B266');
-
-  // Create hover state and set alternative fill color
-  var hs = polygonTemplate.states.create('hover');
-  hs.properties.fill = am4core.color('#367B25');
-
-  // Remove Antarctica
-  polygonSeries.exclude = ['AQ'];
-
-  // Add some data
-  polygonSeries.data = [{
-    'id': 'US',
-    'fill': am4core.color('#F05C5C')
-  }, {
-    'id': 'FR',
-    'fill': am4core.color('#5C5CFF')
-  }];
-
-  // Bind 'fill' property to 'fill' key in data
-  polygonTemplate.propertyFields.fill = 'fill';
-
-  chart.maxZoomLevel = 1;
-  chart.seriesContainer.draggable = false;
-  chart.seriesContainer.resizable = false;
-  chart.seriesContainer.wheelable = false;
-
-
-};
-
-
 
 
