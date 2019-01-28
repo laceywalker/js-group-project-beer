3a414e1017f269e50d79f728cad0e9cef98b6b02
lacey <hiu@example.com>
2019-01-28 15:22:08 +0000

    add createupdatedbeer function

--- client/src/views/beer_view.js
+++ client/src/views/beer_view.js
@@ -1,43 +1,44 @@
 const PubSub = require('../helpers/pub_sub.js');
 
-const BeerView = function(container) {
+const BeerView = function(container, beer) {
   this.container = container;
+  this.beer = beer;
 };
 
 
-BeerView.prototype.render = function(beer) {
+BeerView.prototype.render = function() {
 
   const drankBeerContainer = document.createElement('section');
   drankBeerContainer.id = 'beer';
 
-  const beerName = this.createName(`Beer: ${beer.name}`);
+  const beerName = this.createName(`Beer: ${this.beer.name}`);
   drankBeerContainer.appendChild(beerName);
 
   const beerRatingDesc = this.createDetailList('Rating: ');
   drankBeerContainer.appendChild(beerRatingDesc);
 
-  const beerRating = this.createBeerRating(beer.rating);
+  const beerRating = this.createBeerRating(this.beer.rating);
   drankBeerContainer.appendChild(beerRating);
 
-  const beerBrewery = this.createDetailList(`Brewery: ${beer.brewery}`);
+  const beerBrewery = this.createDetailList(`Brewery: ${this.beer.brewery}`);
   drankBeerContainer.appendChild(beerBrewery);
 
-  const beerAbv = this.createDetailList(`ABV: ${beer.abv}%`);
+  const beerAbv = this.createDetailList(`ABV: ${this.beer.abv}%`);
   drankBeerContainer.appendChild(beerAbv);
 
-  const beerType = this.createDetailList(`Type: ${beer.type}`);
+  const beerType = this.createDetailList(`Type: ${this.beer.type}`);
   drankBeerContainer.appendChild(beerType);
 
-  const beerCountry = this.createDetailList(`Country: ${beer.country}`);
+  const beerCountry = this.createDetailList(`Country: ${this.beer.country}`);
   drankBeerContainer.appendChild(beerCountry);
 
-  const beerDescription = this.createDetailList(`Description: ${beer.description}`);
+  const beerDescription = this.createDetailList(`Description: ${this.beer.description}`);
   drankBeerContainer.appendChild(beerDescription);
 
-  const deleteButton = this.createDeleteButton(beer._id);
+  const deleteButton = this.createDeleteButton();
   drankBeerContainer.appendChild(deleteButton);
 
-  const updateButton = this.createUpdateButton(beer);
+  const updateButton = this.createUpdateButton();
   drankBeerContainer.appendChild(updateButton);
 
   this.container.appendChild(drankBeerContainer)
@@ -56,11 +57,11 @@ BeerView.prototype.createDetailList = function (text) {
   return detail;
 };
 
-BeerView.prototype.createDeleteButton = function (beerId) {
+BeerView.prototype.createDeleteButton = function () {
   const button = document.createElement('button');
   button.classList.add('delete-btn');
   button.textContent = 'Delete';
-  button.value = beerId;
+  button.value = this.beer._id;
 
   button.addEventListener('click', (evt) => {
     PubSub.publish('BeerListView:beer-delete-clicked', evt.target.value);
@@ -70,12 +71,11 @@ BeerView.prototype.createDeleteButton = function (beerId) {
 };
 
 
-BeerView.prototype.createUpdateButton = function (beer) {
+BeerView.prototype.createUpdateButton = function () {
   const button = document.createElement('button');
   button.classList.add('update-btn');
-  const beerToUpdate = beer;
   button.addEventListener('click', (evt) => {
-    PubSub.publish('BeerView:update-button-clicked', beerToUpdate);
+    PubSub.publish('BeerView:update-button-clicked', this.beer);
   });
   return button;
 };
--- client/src/views/edit_view.js
+++ client/src/views/edit_view.js
@@ -3,31 +3,50 @@ const PubSub = require('../helpers/pub_sub.js');
 const EditView = function(divContainer, editForm) {
   this.container = divContainer;
   this.form = editForm;
+  this.beer = null;
 };
 
 
 EditView.prototype.bindEvents = function () {
   PubSub.subscribe('BeerView:update-button-clicked', (evt) => {
     this.container.style.visibility = "visible"
-    const updatedBeer = this.populateEditBox(evt.detail);
-
-
-  });
-  // PubSub.publish('EditView:updated-beer-submit', updatedBeer);
+    this.beer = evt.detail;
+    this.populateEditBox();
+  })
+  this.form.addEventListener('submit', (evt) => {
+    this.handleSubmit(evt);
+  })
       // this.container.style.visibility = "hidden"
-      // PubSub.publish('EditView:updated-beer-submit', updatedBeer);
 };
 
+EditView.prototype.populateEditBox = function () {
+    this.form.name.value = this.beer.name;
+    this.form.brewery.value = this.beer.brewery;
+    this.form.country.value = this.beer.country;
+    this.form.abv.value = this.beer.abv;
+    this.form.type.value = this.beer.type;
+    this.form.description.value = this.beer.description;
+    this.form.rating.value = this.beer.rating
+};
 
-EditView.prototype.populateEditBox = function (beer) {
-  console.table(beer);
-  this.form.name.value = beer.name;
-  this.form.brewery.value = beer.brewery;
-  this.form.country.value = beer.country;
-  this.form.abv.value = beer.abv;
-  this.form.type.value = beer.type;
-  this.form.description.value = beer.description;
-  this.form.rating.value = beer.rating;
+EditView.prototype.handleSubmit = function (evt) {
+  evt.preventDefault();
+  const updatedBeerInput = this.createUpdatedBeer(evt.target);
+  console.log(updatedBeerInput)
+  PubSub.publish('EditView:updated-beer-submit', updatedBeerInput);
+};
+
+EditView.prototype.createUpdatedBeer = function () {
+  const updatedBeer = {
+    name: this.form.name.value,
+    brewery: this.form.brewery.value,
+    country: this.form.country.value,
+    abv: this.form.abv.value,
+    type: this.form.type.value,
+    description: this.form.description.value,
+    rating: this.form.rating.value
+  };
+  return updatedBeer;
 };
 
 
