6dc60a6804b512a8e1464a85f935e932b8c96c9d
lacey <hiu@example.com>
2019-01-30 10:50:43 +0000

    remove console log message

--- client/src/views/random_beer_button_view.js
+++ client/src/views/random_beer_button_view.js
@@ -7,7 +7,6 @@ const RandomBeerButtonView = function (container) {
 
 RandomBeerButtonView.prototype.bindEvents = function () {
   PubSub.subscribe('Beers:data-loaded', () => {
-    console.log("coming in hot");
     const randomBeerButton = document.querySelector('#touch-me');
     randomBeerButton.addEventListener('click', () => {
       PubSub.publish('RandomBeerButtonView:random-beer-clicked');
